7cfd13b6d9ecb6c843e9884954e432fc06558d55
William Bae <wbae912@gmail.com>
2019-09-27 12:24:25 -0700

    ready for deployment

---
+++ server.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const MOVIES = require('./movies.json');
+
+const app = express();
+const morganSettings = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSettings));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).send({ error: 'Unauthorized request' });
+  }
+
+  next();
+});
+
+function handleSearchQuery(req, res) {
+  const { genre, country, avg_vote } = req.query;
+
+  let filteredMovies = MOVIES;
+  if (genre) {
+    filteredMovies = filteredMovies.filter( (movie) => movie.genre.toLowerCase().includes(genre.toLowerCase()));
+  }
+
+  if (country) {
+    filteredMovies = filteredMovies.filter( (movie) => movie.country.toLowerCase().includes(country.toLowerCase()));
+  }
+
+  if (avg_vote) {
+    filteredMovies = filteredMovies.filter( (movie) => movie.avg_vote >= Number(avg_vote));
+  }
+
+  res.json(filteredMovies);
+}
+
+app.get('/movie', handleSearchQuery);
+
+app.use((error,req,res,next) => {
+  let response;
+  if(process.env.NODE_ENV === 'production') {
+    response = { error: {message: 'server error'} };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
